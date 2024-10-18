f6991389a4b1c9ceec5b086ea26b8ba8e5f7afc0
chenzhizsqq <chenzhi@qq.com>
2024-10-18 15:29:37 +0900

    Initial commit

---
+++ RxSwift6/RxSwift6App.swift
@@ -0,0 +1,17 @@
+//
+//  RxSwift6App.swift
+//  RxSwift6
+//
+//  Created by chenzhizs on 2024/10/18.
+//
+
+import SwiftUI
+
+@main
+struct RxSwift6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
